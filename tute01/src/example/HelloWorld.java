5e85d272ba97a3030fe7ed447b216650c3e252bb
Chirag <124755972+sawlani63@users.noreply.github.com>
2026-02-22 16:00:09 +1100

    Add tute01 code

---
+++ tute01/src/example/HelloWorld.java
@@ -0,0 +1,13 @@
+package example;
+
+/**
+ * Prints "Hello World" to the console.
+ *
+ * @author Robert Clifton-Everest
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // TODO Complete this method
+        System.out.println("Hello World");
+    }
+}
